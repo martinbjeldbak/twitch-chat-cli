6deebd49ca25618c032a319e7741388f744869c3
Martin Bjeldbak Madsen <me@martinbjeldbak.com>
2022-04-23 13:48:36 +0000

    Create a chan for each config and write and read to them

--- app/app.go
+++ app/app.go
@@ -11,16 +11,35 @@ type MessageMsg struct {
 	message string
 }
 
+type channel struct {
+	name             string
+	messageChannel   chan MessageMsg // where we will get messages
+	messagesToRender []MessageMsg
+}
+
+type twitchChannelInfos map[string]*channel
+
 type model struct {
-	messages chan MessageMsg // where we will get messages
-	ms       []MessageMsg
-	err      error
+	channels twitchChannelInfos
+
+	err error
+}
+
+type channelMessageMsg struct {
+	name    string
+	message MessageMsg
 }
 
 func (m model) Init() tea.Cmd {
+	var cmds []tea.Cmd
+	for _, c := range m.channels {
+		cmds = append(cmds, waitForMessage(c))
+	}
+
+	cmds = append(cmds, connectTwitch(m.channels))
+
 	return tea.Batch(
-		connectTwitch(m.messages),  // generate messages
-		waitForMessage(m.messages), // wait for a message
+		cmds..., // output messages
 	)
 }
 
@@ -28,21 +47,28 @@ type errMsg struct{ err error }
 
 func (e errMsg) Error() string { return e.err.Error() }
 
-func waitForMessage(sub chan MessageMsg) tea.Cmd {
+func waitForMessage(c *channel) tea.Cmd {
 	return func() tea.Msg {
-		return MessageMsg(<-sub)
+		return channelMessageMsg{name: c.name, message: <-c.messageChannel}
 	}
 }
 
-func connectTwitch(sub chan MessageMsg) tea.Cmd {
+func connectTwitch(ci twitchChannelInfos) tea.Cmd {
 	return func() tea.Msg {
 		client := twitch.NewAnonymousClient()
 
 		client.OnPrivateMessage(func(m twitch.PrivateMessage) {
-			sub <- MessageMsg{message: m.Message}
+			if entry, ok := ci[m.Channel]; ok {
+				entry.messageChannel <- MessageMsg{message: m.Message}
+				ci[m.Channel] = entry
+			}
 		})
 
-		client.Join("nmplol")
+		channelNames := make([]string, 0, len(ci))
+		for k := range ci {
+			channelNames = append(channelNames, k)
+		}
+		client.Join(channelNames...)
 		err := client.Connect()
 
 		if err != nil {
@@ -58,9 +84,12 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 		if k := msg.String(); k == "ctrl+c" || k == "q" || k == "esc" {
 			return m, tea.Quit
 		}
-	case MessageMsg:
-		m.ms = append(m.ms, msg)
-		return m, waitForMessage(m.messages)
+	case channelMessageMsg:
+		channel, ok := m.channels[msg.name]
+		if ok {
+			channel.messagesToRender = append(channel.messagesToRender, msg.message)
+			return m, waitForMessage(channel)
+		}
 	case errMsg:
 		m.err = msg
 		return m, tea.Quit
@@ -74,8 +103,8 @@ func (m model) View() string {
 		return fmt.Sprintf("\nWe had some trouble: %v\n\n", m.err)
 	}
 
-	s := "Messages:\n"
-	for _, msg := range m.ms {
+	s := fmt.Sprintf("Messages for channel %v:\n", m.channels["pgl"].name)
+	for _, msg := range m.channels["pgl"].messagesToRender {
 		s += fmt.Sprintf("\n%v", msg.message)
 	}
 
@@ -85,10 +114,14 @@ func (m model) View() string {
 	return s
 }
 
-func Start() error {
+func Start(channels []string) error {
+	channelInfo := make(twitchChannelInfos)
+	for _, c := range channels {
+		channelInfo[c] = &channel{name: c, messageChannel: make(chan MessageMsg)}
+	}
+
 	p := tea.NewProgram(model{
-		messages: make(chan MessageMsg),
-		ms:       make([]MessageMsg, 0),
+		channels: channelInfo,
 	},
 		tea.WithAltScreen(),
 		tea.WithMouseCellMotion())
