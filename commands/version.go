6654e5d4059ca66bf6720ac656486fd42650d5f6
Martin Bjeldbak Madsen <me@martinbjeldbak.com>
2022-07-17 11:14:47 +0000

    Bump to 0.1

--- commands/version.go
+++ commands/version.go
@@ -15,6 +15,6 @@ var versionCmd = &cobra.Command{
 	Short: "Print the version number of twitch-chat-cli",
 	Long:  "All software has versions. This is Twitch Chat CLI's.",
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("Twitch Chat CLI v0.0 -- HEAD") // TODO: set right version, see https://github.com/gohugoio/hugo/blob/41cc4e4ba3bd849cee7dcb691504ebebbfce680f/common/hugo/version.go#L129
+		fmt.Println("Twitch Chat CLI v0.1 -- HEAD") // TODO: set right version, see https://github.com/gohugoio/hugo/blob/41cc4e4ba3bd849cee7dcb691504ebebbfce680f/common/hugo/version.go#L129
 	},
 }
